63ac861d64e431fdf2baa4c6bf0b2ee7dbc223f1
Shivani <142994273+shivani-1314@users.noreply.github.com>
2024-07-21 19:45:15 +0530

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -8,7 +8,7 @@ import (
 	"testing"
 )
 
-func TestMain(t *testing.T) {
+func TestHomePage(t *testing.T) {
 	req, err := http.NewRequest("GET", "/home", nil)
 	if err != nil {
 		t.Fatal(err)
@@ -24,7 +24,75 @@ func TestMain(t *testing.T) {
 			status, http.StatusOK)
 	}
 
-	// Just verify the code not html content
+	expected := "text/html; charset=utf-8"
+	if contentType := rr.Header().Get("Content-Type"); contentType != expected {
+		t.Errorf("handler returned unexpected content type: got %v want %v",
+			contentType, expected)
+	}
+}
+
+func TestCollectionPage(t *testing.T) {
+	req, err := http.NewRequest("GET", "/collection", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(collectionPage)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expected := "text/html; charset=utf-8"
+	if contentType := rr.Header().Get("Content-Type"); contentType != expected {
+		t.Errorf("handler returned unexpected content type: got %v want %v",
+			contentType, expected)
+	}
+}
+
+func TestAboutPage(t *testing.T) {
+	req, err := http.NewRequest("GET", "/about", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(aboutPage)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expected := "text/html; charset=utf-8"
+	if contentType := rr.Header().Get("Content-Type"); contentType != expected {
+		t.Errorf("handler returned unexpected content type: got %v want %v",
+			contentType, expected)
+	}
+}
+
+func TestContactPage(t *testing.T) {
+	req, err := http.NewRequest("GET", "/contact", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(contactPage)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
 	expected := "text/html; charset=utf-8"
 	if contentType := rr.Header().Get("Content-Type"); contentType != expected {
 		t.Errorf("handler returned unexpected content type: got %v want %v",
